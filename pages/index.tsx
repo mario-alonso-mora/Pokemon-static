4cf10399252950015ee47888c25c91dbeb3a5d20
Mario Alonso Mora <84082175+mario-alonso-mora@users.noreply.github.com>
2023-02-03 16:32:57 +0100

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import { NextPage } from 'next'
+
+
+import React from 'react'
+
+const HomePage:NextPage = () => {
+
+  return (
+    <>
+    
+    <h1>Hola Mundo</h1>
+
+
+    
+    
+    </>
+  )
+}
+
+export default HomePage
